921215ccbd54ab50366bed62f4d92b6b23c06460
manoj.k <117472005+manojk-india@users.noreply.github.com>
2025-06-09 19:13:04 +0530

    First Push
    Instead of asking , doing is better

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chatbot from './components/Chatbot.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
